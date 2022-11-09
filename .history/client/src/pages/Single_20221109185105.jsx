aa8c7d4f4bda5470dd3f8c382f8eaf7fe1ab3704
AldinQ <64334006+AldinQ@users.noreply.github.com>
2022-11-09 18:54:33 +0100

    Komit2

---
+++ .history/client/src/pages/Single_20221109185105.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+const Single = () => {
+  return (
+    <div className='single'>
+      <div className="content">
+        <img src="https://imaes.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <div className="user">
+        <img src="https://images.pexels.com/photos/12463279/pexels-photo-12463279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="info"></div>
+        <span>Jesica</span>
+        <p>Posted 2 days ago</p>
+        </div>
+        
+      </div>
+
+      <div className="menu">m</div>
+
+    </div>
+     
+
+  )
+}
+
+export default Single
