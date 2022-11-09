81d6f91c6f5290c6c9dbdb6b30a1d18665015b8d
AldinQ <64334006+AldinQ@users.noreply.github.com>
2022-11-09 15:41:31 +0100

    Proces stilizovanja SCCS

---
+++ .history/client/src/pages/Single_20221109150835.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+const Single = () => {
+  return (
+    <div className='single'>
+      <div className="content">
+        <img src= "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+      </div>
+      <div className="menu">m</div>
+
+    </div>
+    
+
+  )
+}
+
+export default Single
