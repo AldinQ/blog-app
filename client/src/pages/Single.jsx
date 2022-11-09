a4542629f787e63fe208ff781bbfdf7fddc7ccc9
AldinQ <64334006+AldinQ@users.noreply.github.com>
2022-11-09 18:52:41 +0100

    promena

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -4,8 +4,7 @@ const Single = () => {
   return (
     <div className='single'>
       <div className="content">
-        
-        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <img src="https://imaes.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
         <div className="user">
         <img src="https://images.pexels.com/photos/12463279/pexels-photo-12463279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
         <div className="info"></div>
